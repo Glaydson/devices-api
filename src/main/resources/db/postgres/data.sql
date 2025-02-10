2c0599bcc5ba0537c3ead84cf6b7db0df711b646
glaydson <glaydsonvs@gmail.com>
2025-02-10 20:21:46 +0000

    Rebuild of docker files using docker init. Adding database scripts

---
+++ src/main/resources/db/postgres/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device1', 'BrandA', 'AVAILABLE', '2023-01-01 10:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device2', 'BrandB', 'IN_USE', '2023-01-02 11:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device3', 'BrandC', 'INACTIVE', '2023-01-03 12:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device4', 'BrandD', 'AVAILABLE', '2023-01-04 13:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device5', 'BrandE', 'IN_USE', '2023-01-05 14:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device6', 'BrandF', 'INACTIVE', '2023-01-06 15:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device7', 'BrandG', 'AVAILABLE', '2023-01-07 16:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device8', 'BrandH', 'IN_USE', '2023-01-08 17:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device9', 'BrandI', 'INACTIVE', '2023-01-09 18:00:00');
+INSERT INTO devices (name, brand, state, creation_time) VALUES ('Device10', 'BrandJ', 'AVAILABLE', '2023-01-10 19:00:00');
